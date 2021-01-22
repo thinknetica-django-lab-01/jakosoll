526f8fda60d3013b187c541b71830f07160b43eb
Evgeniy Krivenko <jako.soll@gmail.com>
2021-01-22 23:55:33 +0300

    Created base.html, added nav in base template. Added simple view tests

--- django_lab/main/tests.py
+++ django_lab/main/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import resolve
+from .views import index
 
-# Create your tests here.
+
+class MainViewsTest(TestCase):
+    """testcases for views"""
+    def test_url_resolves_to_index_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+    def test_main_page_uses_right_template(self):
+        """test: main page uses index.html"""
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'index.html')
