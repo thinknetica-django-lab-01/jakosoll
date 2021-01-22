496322996ba487b881ad7350572f792ce527bb0a
Evgeniy Krivenko <jako.soll@gmail.com>
2021-01-22 08:24:37 +0300

    Add flatpages, index.html with links to pages

--- django_lab/django_lab/settings.py
+++ django_lab/django_lab/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
 ]
+
+SITE_ID = 2
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
