9c98ca16cd0d7269325181fae72047d54eaf6a50
Evgeniy Krivenko <jako.soll@gmail.com>
2021-01-26 16:57:10 +0300

    Added turn_on_block and some html with variables in index.html

--- django_lab/main/views.py
+++ django_lab/main/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     """Displays main page"""
-    return render(request, 'index.html')
+    turn_on_block: bool = False
+    return render(request, 'index.html', {'turn_on_block': turn_on_block})
