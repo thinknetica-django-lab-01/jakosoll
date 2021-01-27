bba1eb2d5a5b29c2d752d336a02e86dea8b2f53f
Evgeniy Krivenko <jako.soll@gmail.com>
2021-01-27 23:05:23 +0300

    Added custom tag current time. Not diplays in template

---
+++ django_lab/main/templatetags/server_time.py
@@ -0,0 +1,10 @@
+from django import template
+from datetime import datetime
+
+register = template.Library()
+
+
+@register.simple_tag
+def current_time(format_string):
+    print(datetime.now().strftime(format_string))
+    return datetime.now().strftime(format_string)
