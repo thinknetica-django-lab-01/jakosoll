5faebcddaf8f5070b9ebbc7fc66edc8c4f75471d
Evgeniy Krivenko <jako.soll@gmail.com>
2021-01-31 11:03:02 +0300

    Implemented validation for 'age' field

--- django_lab/main/forms.py
+++ django_lab/main/forms.py
@@ -1,8 +1,17 @@
-from django.forms import ModelForm
+from django import forms
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 
 
-class UpdateUserForm(ModelForm):
+class UpdateUserForm(forms.ModelForm):
+    age = forms.IntegerField()  # можно указать для стандартной валидации min_value=18
+
+    def clean_age(self):
+        """validate age field"""
+        value = self.cleaned_data['age']
+        if value < 18:
+            raise ValidationError('Возраст должен быть больше 18 лет')
+        return value
 
     class Meta:
         model = User
