20685e8e8f09bec114d8a66e857b4a2df0b28ce3
Евгений <jako.soll@gmail.com>
2021-02-01 22:53:43 +0300

    Updated views.py

--- django_lab/main/views.py
+++ django_lab/main/views.py
@@ -44,7 +44,6 @@ class ProductAddView(PermissionRequiredMixin, CreateView):
     permission_required = 'main.add_product'
     model = Product
     form_class = ProductAddForm
-    # login_url = reverse('login')
     template_name = 'product_add.html'
 
     def get_context_data(self, **kwargs):
