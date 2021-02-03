44ec111438d980b058229d000f3b5b3273f212b5
Евгений <jako.soll@gmail.com>
2021-02-04 00:35:12 +0300

    Register product in admin for convenience

--- django_lab/main/admin.py
+++ django_lab/main/admin.py
@@ -4,6 +4,7 @@ from django.contrib.flatpages.models import FlatPage
 from django.utils.translation import gettext_lazy as _
 from ckeditor.widgets import CKEditorWidget
 from django import forms
+from .models import Product
 
 
 class FlatAdminForm(forms.ModelForm):
@@ -25,6 +26,14 @@ class FlatPageAdmin(FlatPageAdmin):
     form = FlatAdminForm
 
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price')
+    list_display_links = ('name', 'category',)
+    list_filter = ('category',)
+    list_editable = ('price',)
+    search_fields = ('name',)
+
+
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
-
+admin.site.register(Product, ProductAdmin)
