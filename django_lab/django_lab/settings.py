f6039f3a885015103282512e91f92abd7ff38a6f
Евгений <jako.soll@gmail.com>
2021-02-09 01:22:19 +0300

    Fixed settings

--- django_lab/django_lab/settings.py
+++ django_lab/django_lab/settings.py
@@ -75,11 +75,11 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 # email settings
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jako.soll@gmail.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER_HOST')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
