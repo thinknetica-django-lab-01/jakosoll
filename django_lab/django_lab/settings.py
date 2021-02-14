f91568640b5edc40093fc86fa392bfb2243c22bb
Евгений <jako.soll@gmail.com>
2021-02-14 12:11:55 +0300

    Setup redis cache

--- django_lab/django_lab/settings.py
+++ django_lab/django_lab/settings.py
@@ -181,3 +181,15 @@ CELERY_RESULT_BACKEND = 'django-db'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
+
+# Caches
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
